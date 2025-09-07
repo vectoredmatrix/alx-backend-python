e8f9e8000e1ae7d0765b355a1740b27b48a2b029
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-07 23:04:41 +0100

    streaming user

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,19 +1,39 @@
-seed = __import__('seed')
+import mysql.connector
+from dotenv import load_dotenv
+import os
 
-def paginate_users(page_size, offset):
-    connection = seed.connect_to_prodev()
+load_dotenv()
+
+def stream_users_in_batches(batch_size):
+    """Stream users from DB in batches."""
+    connection = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        database=os.getenv("DB_NAME", "ALX_prodev"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD")
+    )
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
-    rows = cursor.fetchall()
-    connection.close()
-    return rows
-  
-def lazy_paginate(page_size):
-    offset = 0
-    while True: 
-        page = paginate_users(page_size, offset)
-        if not page:
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
             break
-        yield page
-        offset += page_size
-    return 
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    """Process users in batches and print only those with age > 25."""
+    for user_batch in stream_users_in_batches(batch_size):
+        for user in user_batch:
+            if user and user["age"] > 25:
+                print(user)
+    return
+
+
+if __name__ == "__main__":
+    # Example run with batch size of 5
+    batch_processing(5)
