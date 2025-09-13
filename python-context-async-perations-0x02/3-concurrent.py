7516c08f77c69be48ec0bd2d030c65a5592db5ae
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-14 00:07:55 +0100

    using context manager to read db

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,35 @@
+import asyncio , aiosqlite
+
+
+async def fetch_user(db="" , query = "" , param =()):
+    
+    async with aiosqlite.connect(db) as conn:
+        async with conn.execute(query , param) as cursor:
+           row = await cursor.fetchall()
+           
+           return row
+       
+       
+
+async def fetch_older_user(db = "" , query ="" , params = ()):
+   
+    async with (aiosqlite.connect(db)) as conn:
+        async with conn.execute(query , params) as cursor:
+            result = await cursor.fetchall()
+            
+            return result
+       
+       
+       
+       
+
+async def fetch_concurrently():
+   r = await asyncio.gather(fetch_user(
+       "testdb.sqlite3" , 
+       "SELECT * FROM Users ;"
+   ),
+    fetch_older_user("testdb.sqlite3" , "SELECT * FROM Users WHERE age > ?;" , (40,)))
+   print(r)
+   
+   
+asyncio.run(fetch_concurrently())
