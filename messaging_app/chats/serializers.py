83c285983dafe4331db787ce9cccf7ada4970029
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-16 22:55:36 +0100

    created chat serializer.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,5 +1,6 @@
 from .models import *
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password 
 
 class Users (serializers.ModelSerializer):
     class Meta:
@@ -9,15 +10,32 @@ class Users (serializers.ModelSerializer):
         
     
     def create(self , validated_data):
-        password = validated_data.pop("password")
-        
-        user = User.objects.create(**validated_data)
+        password = validated_data.get("password")
+        validated_data["password"] = make_password(password)
+        # user = User(**validated_data)
         
         #user.set_password(password)
-        user.save()
-        return user
+        #user.save()
+        return super().create(validated_data)
     
     
+class MessageSerial(serializers.ModelSerializer):
+    sender_id = serializers.PrimaryKeyRelatedField(queryset = User.objects.all() , source = "sender_id" , write_only = True)
+    
+    class Meta:
+        model = Message
+        fields = ["message_id" , "message_body","sent_at" ,"sender_id"]
+
+
+class ConvsersionSerial(serializers.ModelSerializer):
+    participant = User(read_only = True , source = "participant_id")
+    participant_id = serializers.PrimaryKeyRelatedField(queryset = User.objects.all() , source = "participant_id" , write_only = True)
+    
+    
+    class Mata:
+        model = Conversation
+        fields = ["conversation_id" , "created_at" , "participant" , "participant_id"]
+        
 
 
 
