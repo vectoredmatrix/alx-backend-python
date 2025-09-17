0427e238bb25952f2651f485fe7057b9ef074342
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-17 21:37:37 +0100

    created chat view set using generics

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+
+
+class ConversationViewset(generics.ListAPIView):
+    queryset = Conversation.objects.all()
+    serializer_class = ConvsersationSerial
+    name = "ConversationViewset"
+    
+
+class MessageViewSet(generics.ListAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerial
+    name = "MessageViewSet"    
