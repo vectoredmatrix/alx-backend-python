89cf8715efa6e570ff8d364911669721ec17743a
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-18 03:29:13 +0100

    created chat serializer.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets , filters , status
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
@@ -7,13 +8,39 @@ from .serializers import *
 # Create your views here.
 
 
-class ConversationViewset(viewsets.ModelViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConvsersationSerial
     name = "ConversationViewset"
+    filter_backends = [filters.SearchFilter]
+    search_fields =  ["particapant__first_name","participant__lanst_name"]
+    
+    def create(self, request, *args, **kwargs):
+        
+        serial = self.get_serializer(data = request.data)
+        if serial.is_valid():
+            self.perform_create(serial)
+            return Response({"Status":"success" , "data":serial.data} , status=status.HTTP_201_CREATED)
+        
+        return Response({"status":"Error" ,"Error":serial.errors } , status=status.HTTP_400_BAD_REQUEST)
+            
     
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerial
-    name = "MessageViewSet"    
+    name = "MessageViewSet"  
+    filter_backends = [filters.SearchFilter]
+    
+    def create(self, request, *args, **kwargs):
+        
+        serial = self.get_serializer(request.data)
+        
+        if serial.is_valid():
+            self.perform_create(serial)
+            
+            return Response({
+                "status":"Success" , "data":serial.data
+            }, status=status.HTTP_201_CREATED)
+            
+        return Response({"status": "error" , "Error":serial.errors} , status=status.HTTP_400_BAD_REQUEST)
