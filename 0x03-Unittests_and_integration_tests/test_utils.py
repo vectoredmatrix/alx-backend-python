ba6428e3011bfdf668fa58ba9438ab8bcdcfdba2
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-21 21:06:32 +0100

    implemented parameterised tesr

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map (Task 0)."""
+"""Unit tests for utils.access_nested_map (Tasks 0 & 1)."""
 
 import unittest
 from parameterized import parameterized
@@ -16,10 +16,26 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
+        Task 0:
         Test that access_nested_map returns the expected result
         for different nested maps and paths.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+        """
+        Task 1:
+        Test that access_nested_map raises KeyError
+        for missing keys, and check the exception message.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_message)
 
 
 if __name__ == "__main__":
