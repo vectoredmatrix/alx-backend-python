1ca4d755d5f8888198a7906b389fa81df2e63dd8
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-27 03:51:03 +0100

    included custom auth

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from rest_framework.permissions import SAFE_METHODS
 class IsParticipantOfConversation(permissions.BasePermission):
     
     def has_permission(self, request, view):
@@ -16,6 +16,9 @@ class IsSenderOfMessage(permissions.BasePermission):
         return request.user and request.user.is_authenticated
     
     def has_object_permission(self, request, view, obj):
+        
+        if request.method in ["PUT" ,"PATCH" ,"DELETE"]:
+            return True
         
         return obj.sender_id == request.user
     
