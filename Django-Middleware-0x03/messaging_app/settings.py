a15f977078a66787da5eb7825045946d2ab9c320
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-29 03:30:29 +0100

    modified middleware ordering

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -74,27 +74,7 @@ SIMPLE_JWT = {
 
 
 
-""" 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    
-    #custom middle wares
-    
-    "chats.middleware.RequestLoggingMiddleware",
-     "chats.middleware.RestrictAccessByTimeMiddleware",
-      "chats.middleware.OffensiveLanguageMiddleware",
-      "chats.middleware.RolepermissionMiddleware",
-    
-]
 
-"""
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
