db3f97e6c0d851fc639f3cd6b4da8497dce6da63
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-10-03 06:46:49 +0100

    created models.py file

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     sender = models.ForeignKey(User , on_delete=models.CASCADE , related_name="sender")
-    reciever = models.ForeignKey(User , on_delete= models.CASCADE , related_name="reciever")
+    receiver = models.ForeignKey(User , on_delete= models.CASCADE , related_name="receiver")
     content = models.TextField(max_length=5000 , blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     
