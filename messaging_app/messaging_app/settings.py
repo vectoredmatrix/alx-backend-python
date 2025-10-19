f355af1541b979d776f3719b459417be51999d29
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-10-19 19:20:24 +0100

    created a deployment file

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -107,17 +107,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("MYSQL_DATABASE"),
-        "USER": os.environ.get("MYSQL_USER"),
-        "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
-        "HOST": "db",  # This matches the 'db' service name in docker-compose.yml
-        "PORT": os.environ.get("MYSQL_PORT", "3306"),
-        "OPTIONS": {
-            "auth_plugin": "mysql_native_password",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
